0cee9d5c1f5ef0c6997545e6ffa576b5ced6b88e
lauechev <laura.echeverri3@gmail.com>
2025-02-21 17:25:47 -0500

    👀 add eye movement animation

--- src/eyes.ts
+++ src/eyes.ts
@@ -2,8 +2,35 @@ const irisLeft = document.querySelector<HTMLDivElement>('div.iris-left');
 const irisRight = document.querySelector<HTMLDivElement>('div.iris-right');
 
 const moveEye = function (tag: HTMLDivElement, mouseX: number, mouseY: number) {
-  tag.style.left = mouseX + 'px';
-  tag.style.top = mouseY + 'px';
+  // tag.style.left = mouseX + 'px';
+  // tag.style.top = mouseY + 'px';
+
+  //center of the eye
+  const eyeMidX = tag.getBoundingClientRect().left;
+  const eyeMidY = tag.getBoundingClientRect().top;
+
+  //find difference between eye and mouse
+  const diffX = mouseX - eyeMidX;
+  const diffY = mouseY - eyeMidY;
+
+  //pythagoras theorem
+  const diff = Math.sqrt(diffX * diffX + diffY * diffY);
+
+  //what is the capped radius
+  const radius = Math.min(3, diff);
+
+  //tan in math
+  const angle = Math.atan2(diffY, diffX);
+
+  //lets get the capped version of this based on the angle
+  const cappedX = radius * Math.cos(angle);
+  const cappedY = radius * Math.sin(angle);
+
+  const eyeTag = tag.querySelector('div');
+  if (eyeTag) {
+    eyeTag.style.left = cappedX + 'px';
+    eyeTag.style.top = cappedY + 'px';
+  }
 };
 
 document.addEventListener('mousemove', function (event) {
