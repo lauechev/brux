6e906074817370c7677647632a38b2c9804bb6e2
lauechev <laura.echeverri3@gmail.com>
2025-02-21 16:33:00 -0500

    🥞 add scroll animation fade in

--- src/animation.ts
+++ src/animation.ts
@@ -5,7 +5,28 @@ animatedTags.forEach((tag) => {
   tag.style.opacity = '0';
 });
 
-const fadeIn = function () {};
+const fadeIn = function () {
+  //look through all the animated tags and see with the getBoundingClientRect if it's in the window.
+
+  let delay = 0.25;
+
+  animatedTags.forEach((tag) => {
+    const tagTop = tag.getBoundingClientRect().top;
+    const tagBottom = tag.getBoundingClientRect().bottom;
+
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      if (tag) {
+        tag.style.animation = `fadein 1s ${delay}s both`;
+        delay = delay + 0.15;
+      }
+    } else {
+      if (tag) {
+        tag.style.opacity = '0';
+        tag.style.animation = '';
+      }
+    }
+  });
+};
 
 //on load, run fadeIn
 fadeIn();
